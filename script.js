66e792c5b60ffa95c7f3ce96c7038666f71f305a
nascimle <78382185+nascimle@users.noreply.github.com>
2021-06-03 07:31:32 -0300

    Update script.js
    Realizada mudanças para deixar o código mais organizado.

--- script.js
+++ script.js
@@ -1,54 +1,32 @@
-
 function calculaTabuada() {
 
-    
 let tabuada = document.querySelector('#tabuada tbody');
-
 let valorA = parseInt(document.querySelector('#valorA').value);
-
-
-
 
 tabuada.innerHTML = '';
 
 
-for(let valorB = 1 ; valorB <= 10; valorB++){
+    for(let valorB = 1 ; valorB <= 10; valorB++){
 
 
-    let resultado = valorA * valorB;
-    
-    let template = `
+        let resultado = valorA * valorB;
 
-            
-
-                <td>${valorA}</td>
-                <td>x</td>
-                <td>${valorB}</td>
-                <td>=</td>
-                <td>${resultado}</td>
-
-            
-
-
-            `;
+        let template = `
+                    <td>${valorA}</td>
+                    <td>x</td>
+                    <td>${valorB}</td>
+                    <td>=</td>
+                    <td>${resultado}</td>`;
 
         let tr = document.createElement('tr');
 
-
-        tr.innerHTML = template;
-
-        tabuada.append(tr);
-
-
-} 
-
-
+            tr.innerHTML = template;
+            tabuada.append(tr);
+     } 
 
 }
 
-
 calculaTabuada();
-
 
 document.querySelector('#valorA').addEventListener('change', calculaTabuada);
 
